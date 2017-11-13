f463589f1585827654ea4d3fa2fa24243cb0d785
Priya PNS <mail2priyanarayanan@gmail.com>
2017-11-13 01:19:57 -0800

    adding basic stub files for all entities that we need in the game

---
+++ src/empire/wars/Bullet.java
@@ -0,0 +1,46 @@
+package empire.wars;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.state.StateBasedGame;
+
+import jig.Entity;
+import jig.Vector;
+import jig.ResourceManager;
+
+public class Bullet extends Entity{
+	private Vector velocity;
+	private String bullet_image;
+	
+	public enum BULLET_TYPE
+	{
+		PLAYER,
+		CREPES,
+		CASTLE
+	}
+	
+	private BULLET_TYPE bullet_type;
+	
+	public Bullet(final float x, final float y, final float vx, final float vy, final String in_bullet_image, final BULLET_TYPE in_bullet_type){
+		super(x,y);
+		this.velocity = new Vector(vx, vy);
+		this.bullet_image = in_bullet_image;
+		this.bullet_type = in_bullet_type;
+		
+		addImageWithBoundingBox(ResourceManager.getImage(this.bullet_image));
+	}
+	
+	public void update(GameContainer container, StateBasedGame game, int delta,
+			int mapWidth, int mapHeight, int tileWidth, int tileHeight){
+		translate(velocity.scale(delta));
+	}
+	
+	public void setVelocity(final Vector v) {
+		velocity = v;
+	}
+
+	public Vector getVelocity() {
+		return velocity;
+	}
+}
